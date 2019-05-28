1d00708865b449be4ca1753af11bc27267289954
Student <example@example.com>
2019-05-28 13:10:33 -0700

    make the stuff work

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { myCode } from './myCode';
+import { hello } from './myCode';
+
+$().ready(function(){
+  hello();
+});
